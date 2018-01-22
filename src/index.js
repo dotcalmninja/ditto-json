cbeecd3c441edd544245edf1182e599081e03ce2
Pim Brouwers <mpbrouwers@gmail.com>
2018-01-22 06:59:40 -0500

    package bump

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ DittoJson.prototype.run = function(files, Ditto, done) {
   setImmediate(done);
 
   Object.keys(files).forEach(function(filepath) {
-    var file = files[filepath],
+    let
+      file = files[filepath],
       json = JSON.parse(file.content);
 
-    if (json) {
-      if (json.template) file.template = json.template;
-      if (json.title) file.title = json.title;
-
+    if (typeof json !== 'undefined' && json !== null) {
       file.content = json;
     }
   });
